e0b9f4573b827b6ba0eb8543d5e9a821f0200e1c
Nababratadeb-NITA <nababrata13@gmail.com>
2023-06-05 01:37:43 +0530

    comment feild(wip)

--- app/post/[id]/page.jsx
+++ app/post/[id]/page.jsx
@@ -1,17 +1,12 @@
 "use client";
 
+import CommentFeild from "@components/CommentFeild";
 import { useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
-
-import Profile from "@components/Profile";
 
 const Post = ({ params }) => {
-  const searchParams = useSearchParams();
-  const userName = searchParams.get("_id");
 
   const [userPost, setUserPost] = useState();
-
-  console.log(userPost);
+  const [comment, setComment] = useState();
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -26,9 +21,8 @@ const Post = ({ params }) => {
 
   return (
     <div className="post-details">
-      <h1 className="text-2xl font-bold mb-4">Post Details</h1>
       {userPost ? (
-        <div className="post-card bg-white rounded shadow-md p-4">
+        <div className="post-card bg-white  rounded shadow-md p-4">
           <h2 className="text-xl font-semibold mb-2">{userPost.prompt}</h2>
           <p className="text-gray-500 mb-2">{userPost.tag}</p>
           {userPost.image && (
@@ -37,11 +31,12 @@ const Post = ({ params }) => {
           <p className="text-gray-700">
             Creator: {userPost.creator.username} ({userPost.creator.email})
           </p>
-          {/* Add any other relevant post details here */}
           <p className="text-gray-700">Likes: {userPost.likes}</p>
           <p className="text-gray-700">Comments: {userPost.comments}</p>
-          {/* Add more details as needed */}
-        </div>
+
+          <CommentFeild comment={comment} setComment={setComment}  />
+
+          </div>
       ) : (
         <p>Loading...</p>
       )}
---
+++ components/CommentFeild.jsx
@@ -0,0 +1,5 @@
+const CommentFeild = ({comment, setComment}) => {
+    return <div>hello world</div>
+}
+
+export default CommentFeild
